97629e4ae05efe230ec605311eb392b4bb22a59b
alexs <alexs@SNABBEBOXEN>
2020-01-01 16:03:11 +0100

    Added First Project

--- Day1-Counter/Program.cs
+++ Day1-Counter/Program.cs
@@ -7,6 +7,7 @@ namespace Day1_Counter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
